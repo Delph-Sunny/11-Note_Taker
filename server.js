0b33afc3a26d235a613b9d7eb423565bba122a3e
Delph <delph_teb@hotmail.com>
2021-01-21 01:10:48 -0500

    add package and server file

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require("express");
+
+// EXPRESS CONFIGURATION
+const app = express();
+
+// Sets an initial port
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: http://127.0.0.1:" + PORT);
+});
