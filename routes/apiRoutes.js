c2707b221b0acd90fc955f3b819179bd3a56a835
Delph <delph_teb@hotmail.com>
2021-01-23 00:46:20 -0500

    api delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
   }));
 
   app.get("/api/notes", function (req, res) {
-    
+
     return res.json(objectsList)
   })
 
@@ -17,27 +17,44 @@ module.exports = function (app) {
 
   // API POST Requests
   app.post("/api/notes", function (req, res) {
-    let newNote = { title: req.body.title, text: req.body.text};
+    let newNote = { title: req.body.title, text: req.body.text };
     newNote.id = objectsList.length.toString(); // convert to string and add to note
-    
-    console.log("my new object:", newNote);    // FOR TESTING    
-    console.log("List of the objects in API:", objectsList); // FOR TESTING 
-    
-    objectsList.push(newNote);
-    console.log("List of old with new objects:", objectsList);    // FOR TESTING
 
-  fs.writeFile("./db/db.json",  JSON.stringify(objectsList), (err) => {
-  if (err) throw err;
-  });
+    //  console.log("my new object:", newNote);    // FOR TESTING    
+    //  console.log("List of the objects in API:", objectsList); // FOR TESTING 
+
+    objectsList.push(newNote);
+    //  console.log("List of old with new objects:", objectsList);    // FOR TESTING
+
+    fs.writeFile("./db/db.json", JSON.stringify(objectsList), (err) => {
+      if (err) throw err;
+    });
     res.json(objectsList);
   });
 
   // API DELETE Requests  
   app.delete("/api/notes/:id", function (req, res) {
-   var deleteId = JSON.parse(req.params.id);
-   // objectsList= objectsList.filter( id => id == deleteId);
-   // console.log(objectsList)
+    var idSelected = JSON.parse(req.params.id);
+    console.log(idSelected)
 
-    res.end("Deleted!");
+   objectsList = objectsList.filter((e)=>{
+      console.log(e.id);    // FOR TESTING
+      return e.id != idSelected;
+    });
+    console.log("List of the objects in API:", objectsList);    // FOR TESTING
+
+    // TO DO: reassign object.id to array index of objectsList
+
+    fs.writeFile("./db/db.json", JSON.stringify(objectsList), (err) => {
+      if (err) throw err;
+    });
+    res.end(); // Shorten the response
   });
+
+// MAYBE For future:
+/* API PUT Requests 
+app.put("/api/notes/:id", function (req, res) {
+  var idSelected = JSON.parse(req.params.id);
+*/
+
 };
