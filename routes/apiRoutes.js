76df2369698168805d1b08131ad3f9e2600e9b97
Delph <delph_teb@hotmail.com>
2021-01-22 23:53:24 -0500

    fix bug in post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
   }));
 
   app.get("/api/notes", function (req, res) {
-    console.log("List of the objects in API:", objectsList); // FOR TESTING
+    
     return res.json(objectsList)
   })
 
@@ -17,24 +17,26 @@ module.exports = function (app) {
 
   // API POST Requests
   app.post("/api/notes", function (req, res) {
-    let newNote = req.body;
+    let newNote = { title: req.body.title, text: req.body.text};
     newNote.id = objectsList.length.toString(); // convert to string and add to note
-    console.log(newNote);    // FOR TESTING 
-    console.log(objectsList);    // FOR TESTING 
-    var savedNote = objectsList.push(newNote);
-    console.log(savedNote);    // FOR TESTING
+    
+    console.log("my new object:", newNote);    // FOR TESTING    
+    console.log("List of the objects in API:", objectsList); // FOR TESTING 
+    
+    objectsList.push(newNote);
+    console.log("List of old with new objects:", objectsList);    // FOR TESTING
 
-    fs.writeFile("./db/db.json", JSON.stringify(savedNote), "utf8", (err) => {
-      if (err) throw err;
-    });
-    res.json(savedNote);
+  fs.writeFile("./db/db.json",  JSON.stringify(objectsList), (err) => {
+  if (err) throw err;
+  });
+    res.json(objectsList);
   });
 
   // API DELETE Requests  
   app.delete("/api/notes/:id", function (req, res) {
-    var deleteId = JSON.parse(req.params.id);
-
-    // console.log(objectsList)
+   var deleteId = JSON.parse(req.params.id);
+   // objectsList= objectsList.filter( id => id == deleteId);
+   // console.log(objectsList)
 
     res.end("Deleted!");
   });
